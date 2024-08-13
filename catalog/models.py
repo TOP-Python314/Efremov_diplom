84a8fc89f6941fdbe4c32aecc09b968cb96bb15b
Danis Art <nikita.efremov1412@gmail.com>
2024-08-13 18:54:31 +0300

    Доработка

--- catalog/models.py
+++ catalog/models.py
@@ -1,30 +1,38 @@
 from django.db import models
-from django.db.models import SmallAutoField, DecimalField, CharField, ManyToManyField
+from django.db.models import SmallAutoField, DecimalField, CharField, ManyToManyField, ImageField, TextField
 
 # Create your models here.
 class Product(models.Model):
     id = SmallAutoField(primary_key=True)
     name = CharField(max_length=50)
+    images = models.CharField(max_length=255, null=True)
+    text = TextField(null=True)
     proteins = DecimalField(max_digits=5, decimal_places=2)
     fats = DecimalField(max_digits=5, decimal_places=2)
     carbs = DecimalField(max_digits=5, decimal_places=2)
     kcals = DecimalField(max_digits=5, decimal_places=2)
+    categorys = CharField(max_length=50, null=True)
+    areas = CharField(max_length=50, null=True)
     
     class Meta:
         db_table = 'products'
 
     def __repr__(self):
-        return f'{id}\n{name}\n{kcals}\n{proteins}\n{fats}\n{carbs}'
+        return f'{self.id}\n{self.name}\n{self.kcals}\n{self.proteins}\n{self.fats}\n{self.carbs}'
         
 
 
 class Dish(models.Model):
  
     name = CharField(max_length=50, unique=True)
+    images = models.CharField(max_length=255, null=True)
+    recipes = TextField(null=True)
     proteins = DecimalField(max_digits=5, decimal_places=2)
     fats = DecimalField(max_digits=5, decimal_places=2)
     carbs = DecimalField(max_digits=5, decimal_places=2)
     kcals = DecimalField(max_digits=5, decimal_places=2)
+    categorys = CharField(max_length=50, null=True)
+    areas = CharField(max_length=50, null=True)
     
     products = ManyToManyField('Product', db_table='dishes_products')
     
@@ -32,4 +40,4 @@ class Dish(models.Model):
         db_table = 'dishes'
         
     def __repr__(self):
-        return f'{name}\n{kcals}\n{proteins}\n{fats}\n{carbs}'
+        return f'{self.name}\n{self.kcals}\n{self.proteins}\n{self.fats}\n{self.carbs}'
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
@@ -12,10 +12,9 @@ from . import models
 # from . import forms
 
 def main(request):
-    # templ = (settings.BASE_DIR / 'templates/head.html').read_text(encoding='utf-8')
-    
+    # templ = (settings.BASE_DIR / 'templates/head.html').read_text(encoding='utf-8')   
     # return HttpResponse(templ)
-    return render(request, 'head.html')
+    return render(request, 'index.html')
     
 # def products(request):
     # html_dock = TemplateResponse(
@@ -31,4 +30,55 @@ def products(request):
     return render(request, 'products.html', {'products': products_list})
    
 def calculator(request):
+    products_list = models.Product.objects.all()
+    return render(request, 'calculator.html', {'products': products_list})
+    
+def profile(request):
+    return render(request, 'profile.html')
+
+def dishes(request):
+    added_dishes = Dish.objects.filter(type='added')  # Получаем все блюда из базы данных
+    created_dishes = Dish.objects.filter(type='created')  # Получаем созданные блюда
+
+    if request.method == 'POST':  # Обработка формы создания нового блюда
+        name = request.POST.get('name')
+        image = request.POST.get('image')  # Загрузка файла
+        proteins = request.POST.get('proteins')
+        fats = request.POST.get('fats')
+        carbs = request.POST.get('carbs')
+        kcals = request.POST.get('kcals')
+        areas = request.POST.get('areas')
+        categorys = request.POST.get('categorys')
+        recipes = request.POST.get('recipes')
+
+        # Создаем новое блюдо и сохраняем его в базе данных
+        new_dish = Dish(
+            name=name,
+            image=image,
+            proteins=proteins,
+            fats=fats,
+            carbs=carbs,
+            kcals=kcals,
+            areas=areas,
+            categorys=categorys,
+            recipes=recipes
+        )
+        new_dish.save()
+
+        # Перенаправление на ту же страницу
+        return HttpResponseRedirect(reverse('saved_dishes'))
+
+    # Возвращаем рендеринг, если метод не POST
+    return render(request, 'saved_dishes.html', {
+        'added_dishes': added_dishes,
+        'created_dishes': created_dishes,
+    })
+def created_dishes():
     ...
+    
+def added_dishes():
+    ...
+
+    
+def saved_dishes(request):
+    return render(request, 'saved_dishes.html')
